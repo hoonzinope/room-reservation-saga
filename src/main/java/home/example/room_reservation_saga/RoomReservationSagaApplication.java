e0a1979f33ea006025952b8c991180af3a107b97
hoonzinope <hoonzinope@gmail.com>
2026-06-20 16:22:41 +0900

    first commit

---
+++ src/main/java/home/example/room_reservation_saga/RoomReservationSagaApplication.java
@@ -0,0 +1,13 @@
+package home.example.room_reservation_saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationSagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/example/room_reservation_saga/RoomReservationSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package home.example.room_reservation_saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
