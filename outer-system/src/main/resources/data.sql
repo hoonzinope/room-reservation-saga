4d9314d68a276ac8aa08cb3e599fde4f91114a07
hoonzinope <hoonzinope@gmail.com>
2026-06-20 17:35:38 +0900

    feat : outer 실행을 위한 프로퍼티, 데이터 구성

---
+++ outer-system/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO rooms (room_number, room_type, status)
+VALUES
+    ('101', 'STANDARD', 'AVAILABLE'),
+    ('102', 'STANDARD', 'AVAILABLE'),
+    ('201', 'DELUXE', 'AVAILABLE'),
+    ('202', 'DELUXE', 'MAINTENANCE'),
+    ('301', 'SUITE', 'AVAILABLE');
