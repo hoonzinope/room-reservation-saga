91cc0821eab3bc50fcf94c8c0e77c3514fcf0c4e
hoonzinope <hoonzinope@gmail.com>
2026-06-20 17:17:56 +0900

    feat : 멀티 프로젝트 구조 변경
    - inner, outer system으로 분리
    - root src 삭제

---
+++ inner-system/src/main/java/home/example/room_reserve_inner/RoomReservationInnerApplication.java
@@ -0,0 +1,11 @@
+package home.example.room_reserve_inner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationInnerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RoomReservationInnerApplication.class, args);
+    }
+}
---
+++ outer-system/src/main/java/home/example/room_reserve_outer/RoomReservationOuterApplication.java
@@ -0,0 +1,11 @@
+package home.example.room_reserve_outer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationOuterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RoomReservationOuterApplication.class, args);
+    }
+}
